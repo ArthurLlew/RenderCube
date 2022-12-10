556743fc26d9366f89b5e8bfe2ac4faea6ae132e
Dreadoom <raakl.mam@gmail.com>
2022-12-10 22:41:19 +0300

    Reverted vertex coordinates precision tweaks

--- src/main/java/dreadoom/render_cube/rendered_entities/RenderedVertex.java
+++ src/main/java/dreadoom/render_cube/rendered_entities/RenderedVertex.java
@@ -7,17 +7,17 @@ public class RenderedVertex {
     /**
      * Vertex x coordinate.
      */
-    public float x;
+    public double x;
 
     /**
      * Vertex y coordinate.
      */
-    public float y;
+    public double y;
 
     /**
      * Vertex z coordinate.
      */
-    public float z;
+    public double z;
 
     /**
      * Vertex u coordinate.
@@ -38,21 +38,9 @@ public class RenderedVertex {
      * @param v vertex v coordinate
      */
     public RenderedVertex(double x, double y, double z, float u, float v){
-        // All coordinates have to be >= 0.0001 and do not have more than 4 numbers after decimal point in order for
-        // blender to read them properly
-        this.x = (float)(Math.floor(x * 10000.0) / 10000.0);
-        if (this.x == 0 && x != 0)
-            this.x = (float)(Math.signum(x) * 0.0001);
-
-        this.y = (float)(Math.floor(y * 10000.0) / 10000.0);
-        if (this.y == 0 && y != 0)
-            this.y = (float)(Math.signum(y) * 0.0001);
-
-        this.z = (float)(Math.floor(z * 10000.0) / 10000.0);
-        if (this.z == 0 && z != 0)
-            this.z = (float)(Math.signum(z) * 0.0001);
-
-        // This does not apply to UVs tho
+        this.x = x;
+        this.y = y;
+        this.z = z;
         this.u = u;
         this.v = v;
     }

9e62007c3887b16b150989bd6a8fd0716add6ac7
Dreadoom <raakl.mam@gmail.com>
2024-02-14 19:30:00 +0300

    Removed unused item

--- src/main/java/com/render_cube/RenderCube.java
+++ src/main/java/com/render_cube/RenderCube.java
@@ -37,21 +37,6 @@ public class RenderCube
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Items which will all be registered under the MODID namespace
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the MODID namespace
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
-
-    // Creates a new food item with the id "MODID:rendercube_item", nutrition 1 and saturation 2
-    public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("rendercube_item", () -> new Item(new Item.Properties().food(new FoodProperties.Builder()
-            .alwaysEat().nutrition(1).saturationMod(2f).build())));
-
-    // Creates a creative tab with the id "MODID:rendercube_tab" for the example item, that is placed after the combat tab
-    public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("rendercube_tab", () -> CreativeModeTab.builder()
-            .withTabsBefore(CreativeModeTabs.COMBAT)
-            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
-            .displayItems((parameters, output) -> {
-                output.accept(EXAMPLE_ITEM.get()); // Add the item to the tab.
-            }).build());
 
     /**
      * Mod init.
@@ -63,11 +48,6 @@ public class RenderCube
 
         // Register the commonSetup method for mod loading
         modEventBus.addListener(this::commonSetup);
-
-        // Register the Deferred Register to the mod event bus so items get registered
-        ITEMS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so tabs get registered
-        CREATIVE_MODE_TABS.register(modEventBus);
 
         // Register mod for server and other game events
         MinecraftForge.EVENT_BUS.register(this);

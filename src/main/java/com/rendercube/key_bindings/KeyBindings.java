6d8e7c60243587e29d1aba71961750d49ae756f6
ArthurLlew <kiseliov.evgeniy.iv@yandex.ru>
2024-06-21 15:00:40 +0300

    Imports cleanup

--- src/main/java/com/rendercube/key_bindings/KeyBindings.java
+++ src/main/java/com/rendercube/key_bindings/KeyBindings.java
@@ -1,6 +1,5 @@
 package com.rendercube.key_bindings;
 
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;

b488a1205a40ddaa583a9fce086df2aa9c917e43
ArthurLlew <kiseliov.evgeniy.iv@yandex.ru>
2024-12-21 17:55:30 +0300

    Code refactor

--- src/main/java/com/rendercube/key_bindings/KeyBindings.java
+++ src/main/java/com/rendercube/key_bindings/KeyBindings.java
@@ -15,11 +15,11 @@ public final class KeyBindings {
     public static final String MOD_KEY_CATEGORY = "key.category." + MODID + ".rendercube";
 
     public final KeyMapping RENDER_SCREEN_KEY = new KeyMapping("key." + MODID + ".render_screen",
-            KeyConflictContext.IN_GAME,
-            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_R,
-            MOD_KEY_CATEGORY);
+                                                                KeyConflictContext.IN_GAME,
+                                                                InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_R,
+                                                                MOD_KEY_CATEGORY);
     public final KeyMapping DUMP_TEXTURES_KEY = new KeyMapping("key." + MODID + ".dump_textures",
-            KeyConflictContext.IN_GAME,
-            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_BACKSLASH,
-            MOD_KEY_CATEGORY);
+                                                                KeyConflictContext.IN_GAME,
+                                                                InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_BACKSLASH,
+                                                                MOD_KEY_CATEGORY);
 }

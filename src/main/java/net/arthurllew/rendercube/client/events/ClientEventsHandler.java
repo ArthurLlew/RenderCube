b0683ccfe6caef8af337c4169fabae7ef2355fe3
ArthurLlew <non.fatum.mori@gmail.com>
2026-02-09 02:12:50 +0300

    Code refactor

--- src/main/java/net/arthurllew/rendercube/client/events/ClientEventsHandler.java
+++ src/main/java/net/arthurllew/rendercube/client/events/ClientEventsHandler.java
@@ -1,13 +1,13 @@
 package net.arthurllew.rendercube.client.events;
 
 import net.arthurllew.rendercube.RenderCube;
-import net.arthurllew.rendercube.client.KeyBindings;
+import net.arthurllew.rendercube.client.keyboard.KeyBindings;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-import static net.arthurllew.rendercube.client.KeyInputReactions.openRenderScreen;
+import static net.arthurllew.rendercube.client.keyboard.KeyInputReactions.openRenderScreen;
 
 @Mod.EventBusSubscriber(modid = RenderCube.MODID, value = Dist.CLIENT)
 public class ClientEventsHandler {
--- src/main/java/net/arthurllew/rendercube/client/keyboard/KeyBindings.java
+++ src/main/java/net/arthurllew/rendercube/client/keyboard/KeyBindings.java
@@ -1,4 +1,4 @@
-package net.arthurllew.rendercube.client;
+package net.arthurllew.rendercube.client.keyboard;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;

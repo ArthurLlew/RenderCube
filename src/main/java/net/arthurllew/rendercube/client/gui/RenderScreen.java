b358218bb8b903ccf6b3864d70e09a8799d1a39e
ArthurLlew <non.fatum.mori@gmail.com>
2026-02-13 15:18:25 +0300

    Fixed region size limit message

--- src/main/java/net/arthurllew/rendercube/client/gui/RenderScreen.java
+++ src/main/java/net/arthurllew/rendercube/client/gui/RenderScreen.java
@@ -25,6 +25,7 @@ import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 
 import java.util.List;
+import java.util.function.Supplier;
 
 import static net.arthurllew.rendercube.RenderCube.MODID;
 import static net.arthurllew.rendercube.client.rendering.CubesRenderer.renderRegion;
@@ -56,8 +57,9 @@ public class RenderScreen extends Screen {
             Component.translatable("gui." + MODID + ".render_screen.button.render");
     private static final Component RENDER_WRONG_INPUT_MSG =
             Component.translatable("gui." + MODID + ".render_screen.button.render.wrong_input");
-    private static final Component RENDER_REGION_TOO_LARGE_MSG =
-            Component.translatable("gui." + MODID + ".render_screen.button.render.region_too_large");
+    private static final Supplier<Component> RENDER_REGION_TOO_LARGE_MSG = () ->
+            Component.translatable("gui." + MODID + ".render_screen.button.render.region_too_large",
+                    Config.DATA.maxRenderDistance);
     private static final Component RENDER_SUCCESS_MSG =
             Component.translatable("gui." + MODID + ".render_screen.button.render.success");
     private static final Component RENDER_ERROR_MSG =
@@ -317,7 +319,7 @@ public class RenderScreen extends Screen {
 
                 // Restrict region size
                 if ((maxX - minX > Config.DATA.maxRenderDistance) || (maxZ - minZ > Config.DATA.maxRenderDistance)) {
-                    player.sendSystemMessage(RENDER_REGION_TOO_LARGE_MSG);
+                    player.sendSystemMessage(RENDER_REGION_TOO_LARGE_MSG.get());
                 }
                 else {
                     // Min/max positions in region

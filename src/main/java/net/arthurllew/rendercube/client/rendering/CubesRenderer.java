ab35fb0dd91aef187a935cb046b9e95e8b6e0e79
ArthurLlew <non.fatum.mori@gmail.com>
2026-04-03 19:45:29 +0300

    Config update

--- src/main/java/net/arthurllew/rendercube/client/rendering/CubesRenderer.java
+++ src/main/java/net/arthurllew/rendercube/client/rendering/CubesRenderer.java
@@ -48,7 +48,7 @@ public class CubesRenderer {
             for (Config.Data.BlockConsumerConfig blockConsumerConfig : Config.DATA.blockConsumerConfigs) {
                 // Try to get custom settings
                 Config.Data.BlockConsumerConfig.BlockConsumerSettings settings =
-                        blockConsumerConfig.getSettings(blockState);
+                        blockConsumerConfig.getSettings(level, blockState, levelPos);
                 if (settings != null) {
                     // Assign new values and stop loop
                     blockConsumerSettings = settings;
